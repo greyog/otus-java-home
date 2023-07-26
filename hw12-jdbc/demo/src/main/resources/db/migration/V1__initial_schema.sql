d2a6664e3849970a2a444b330a616163a2506819
greyog <greyog@gmail.com>
2023-07-26 23:03:46 +0300

    some sql methods

--- hw12-jdbc/demo/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw12-jdbc/demo/src/main/resources/db/migration/V1__initial_schema.sql
@@ -8,10 +8,3 @@ create table client
     id   bigserial not null primary key,
     name varchar(50)
 );
-create table manager
-(
-    no   bigserial not null primary key,
-    label varchar(50)
-    param varchar(50)
-);
-
--- hw12-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
+++ hw12-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -9,6 +9,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,12 +56,51 @@ public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     @Override
     public List<T> findAll(Connection connection) {
-        throw new UnsupportedOperationException();
+        return dbExecutor.executeSelect(connection,
+                entitySQLMetaData.getSelectAllSql(),
+                Collections.emptyList(),
+                rs -> {
+                    List<T> resultList = new ArrayList<>();
+                    try {
+                        while (rs.next()) {
+                            var fieldsValues = new ArrayList<Object>();
+                            for (Field field : entityClassMetaData.getAllFields()) {
+                                fieldsValues.add(rs.getObject(field.getName(), field.getType()));
+                            }
+                            var instance = entityClassMetaData.getConstructor().newInstance(fieldsValues.toArray());
+                            resultList.add(instance);
+                        }
+                    } catch (SQLException e) {
+                        throw new DataTemplateException(e);
+                    } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
+                        e.printStackTrace();
+                    }
+                    return resultList;
+                }).orElseThrow(() -> new RuntimeException("Unexpected error"));
     }
 
     @Override
     public long insert(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+        try {
+            return dbExecutor.executeStatement(connection,
+                    entitySQLMetaData.getInsertSql(),
+                    entityClassMetaData.getAllFields().stream()
+                            .map(field -> {
+                                field.setAccessible(true);
+                                Object value = null;
+                                try {
+                                    value = field.get(client);
+                                } catch (IllegalAccessException e) {
+                                    e.printStackTrace();
+                                } finally {
+                                    field.setAccessible(false);
+                                }
+                                return value;
+                            })
+                            .toList());
+        } catch (Exception e) {
+            throw new DataTemplateException(e);
+        }
     }
 
     @Override
