259b42f0c8415d656dd94cca55d3eb69b226d108
serg.volkov <serg.volkov@tinkoff.ru>
2023-09-29 16:59:09 +0300

    spring app initializer

---
+++ hw15-web-server/src/main/java/ru/utus/hw/atmwebservice/AtmWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.utus.hw.atmwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmWebServiceApplication.class, args);
+	}
+
+}
---
+++ hw15-web-server/src/test/java/ru/utus/hw/atmwebservice/AtmWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.utus.hw.atmwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
